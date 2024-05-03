290c58ad8ebbd904a98799d99eba5568038b41d5
PandaEMOnium <99126056+Wanli-Go@users.noreply.github.com>
2024-05-03 21:37:38 +0800

    added h2&h3 support

--- nytimes.js
+++ nytimes.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const url = 'https://www.nytimes.com/2024/05/02/science/orangutan-wound-plant-treatment.html';
+const url = 'https://www.nytimes.com/2024/04/06/technology/ai-data-tech-companies.html';
 
 axios.get(url, {   proxy: {
     protocol: 'http',
-    host: '127.0.0.1',  // Proxy server host name export https_proxy=http://127.0.0.1:33210 http_proxy=http://127.0.0.1:33210 all_proxy=socks5://127.0.0.1:33211
-    port: 33210,                        // Proxy server port
+    host: '127.0.0.1',  // Change to your Proxy server host name 
+    port: 33210,        // Change to your Proxy server port
   },
   headers: {
     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
@@ -18,21 +18,26 @@ axios.get(url, {   proxy: {
     const $ = cheerio.load(html);
     const articleContent = [];
 
-    $('section[name="articleBody"] .css-53u6y8 p').each((_, element) => {
-      let paragraph = $(element).text();
-      const links = $(element).find('a');
-
-      if (links.length > 0) {
-        links.each((_, link) => {
-          const text = $(link).text();
-          const href = $(link).attr('href');
-          const markdownLink = `[${text}](${href})`;
-          paragraph = paragraph.replace(text, markdownLink);
-        });
-      }
-
-      articleContent.push(paragraph);
-    });
+    $('section[name="articleBody"] .css-53u6y8').children().each((_, element) => {
+        if ($(element).is('h2, h3, p')) {
+          let content = '';
+          if ($(element).is('p')) {
+            content = $(element).text();
+          } else {
+            content = `### ${$(element).text()}`;
+          }
+          const links = $(element).find('a');
+          if (links.length > 0) {
+            links.each((_, link) => {
+              const text = $(link).text();
+              const href = $(link).attr('href');
+              const markdownLink = `[${text}](${href})`;
+              content = content.replace(text, markdownLink);
+            });
+          }
+          articleContent.push(content);
+        }
+      });
 
     const markdown = articleContent.join('\n\n');
     console.log(markdown);
